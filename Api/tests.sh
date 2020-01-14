929f7fffbb646f8269e22184f689bfc5f8b87855
Alexander Ivanov <avi2011class@yandex.ru>
2020-01-14 15:04:47 +0300

    update tests run

--- Api/tests.sh
+++ Api/tests.sh
@@ -29,5 +29,5 @@ echo 'DB IS INITIALISED, READY TO START TESTS'
 
 coverage run -m pytest -s tests/*.py
 status_code=$?
-coverage report -m
+coverage report -m --omit="tests/*","__init__.py" --include="**/*.py"
 exit $status_code
